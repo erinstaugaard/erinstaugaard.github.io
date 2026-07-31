5053d853ad2b81670010a357c954b9e1cfd3f668
Erin Staugaard <astaugaard@icloud.com>
2026-07-31 10:31:13 -0400

    fixed style link and blog return url

--- src/main.rs
+++ src/main.rs
@@ -152,13 +152,13 @@ fn main() -> Result<()> {
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
     <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\
     <title>My Website</title>\
-    <link rel=\"stylesheet\" href=\"style.css\">\
+    <link rel=\"stylesheet\" href=\"../style.css\">\
     <link rel=\"icon\" href=\"./favicon.ico\" type=\"image/x-icon\">\
   </head>\
   <body>\
     <nav class=\"navbar\">\
         <a href=\"/\">Home</a>\
-        <a href=\"blog\">Blog</a>\
+        <a href=\"/blog\">Blog</a>\
     </nav>\
     <div class=\"content\">\
       <h1>Posts</h1>\
